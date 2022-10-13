471900c52530284f09384437d2bd0d726a33ffcb
fcojavierrodriguezcanal <95092710+fcojavierrodriguezcanal@users.noreply.github.com>
2022-10-13 20:47:35 +0200

    nodenative

---
+++ 1TRIMESTRE/06-nodejs-native/decompose.js
@@ -0,0 +1,7 @@
+function decomposeUrl(url) {
+	const pagina=url;
+    pagina.protocol;
+    console.log(pagina.protocol);
+
+}
+decomposeUrl("https://docs.google.com/");
---
+++ 1TRIMESTRE/06-nodejs-native/leerfichero/leerfichero.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const archivo=process.argv[2];
+try {
+const data = fs.readFileSync(archivo, 'utf8')
+console.log(data)
+} catch (err) {
+console.error(err)
+}
